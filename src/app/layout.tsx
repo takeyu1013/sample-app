178c88197f5c2d51e104cd868979df535bf4f829
takeyu1013 <yuto1013@gmail.com>
2025-04-26 04:04:18 +0000

    💄

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,22 @@
 import {
+	Anchor,
 	AppShell,
 	AppShellHeader,
 	AppShellMain,
 	ColorSchemeScript,
 	Container,
+	Group,
 	MantineProvider,
-	Stack,
-	Title,
 	mantineHtmlProps,
 } from "@mantine/core";
 import "@mantine/core/styles.css";
 import type { Metadata } from "next";
-import type { ReactNode } from "react";
+import Link from "next/link";
+import type { ComponentProps, ReactNode } from "react";
 
 import { fullTitle } from "@/lib/service";
+
+import classes from "./_layout.module.css";
 
 export const metadata = {
 	title: fullTitle(),
@@ -32,11 +35,40 @@ export default function RootLayout({
 					<AppShell header={{ height: 50, offset: true }}>
 						<AppShellHeader bg="dark">
 							<Container h="100%" size="lg">
-								<Stack h="100%" justify="center">
-									<Title c="#fff" order={1} size="h2">
+								<Group h="100%" justify="space-between">
+									<Anchor
+										c="white"
+										component={Link}
+										href="/"
+										style={{ fontSize: 23.8, fontWeight: 700 }}
+										underline="never"
+									>
 										SAMPLE APP
-									</Title>
-								</Stack>
+									</Anchor>
+									<Group gap="xl">
+										{(
+											[
+												["Home", "/"],
+												["Help", "#"],
+												["Log in", "#"],
+											] satisfies [
+												string,
+												ComponentProps<typeof Link>["href"],
+											][]
+										).map(([name, href]) => (
+											<Anchor
+												className={classes.anchor}
+												component={Link}
+												key={name}
+												href={href}
+												size="sm"
+												underline="never"
+											>
+												{name}
+											</Anchor>
+										))}
+									</Group>
+								</Group>
 							</Container>
 						</AppShellHeader>
 						<AppShellMain>
