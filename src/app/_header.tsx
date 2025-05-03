03f9356bfc63156f55075a692021a438765b97d5
takeyu1013 <yuto1013@gmail.com>
2025-05-03 14:21:40 +0000

    💄

--- src/app/_header.tsx
+++ src/app/_header.tsx
@@ -4,7 +4,6 @@ import {
 	Box,
 	Burger,
 	Container,
-	Flex,
 	Group,
 	Menu,
 	MenuDropdown,
@@ -28,50 +27,54 @@ export const Header = () => {
 	return (
 		<AppShellHeader bg="dark">
 			<Container h="100%" size="lg">
-				<Flex align="center" h="100%" rowGap="xs" justify="space-between">
+				<Group h="100%" justify="space-between">
 					<Logo />
 					<Box component="nav">
-						<Menu width="100%">
-							<MenuTarget>
-								<Burger color="white" hiddenFrom="sm" size="sm" />
-							</MenuTarget>
-							<MenuDropdown
-								px="md"
-								py="xs"
-								bg="dark"
-								left={0}
-								style={{
-									border: "none",
-									borderTop: "1px solid var(--mantine-color-dark-2)",
-									borderRadius: 0,
-								}}
-							>
-								{staticMenuList.map(([name, href]) => (
-									<MenuItem
-										className={classes.menuItem}
-										component="li"
-										key={name}
-										px={0}
-										py="xs"
-										style={{ listStyle: "none" }}
-									>
-										<Anchor
-											className={classes.headerAnchor}
-											component={Link}
-											display="block"
-											href={href}
-											size="sm"
-											underline="never"
+						<Box hiddenFrom="sm">
+							<Menu width="100%">
+								<MenuTarget>
+									<Burger color="white" size="sm" />
+								</MenuTarget>
+								<MenuDropdown
+									bg="dark"
+									component="ul"
+									left={0}
+									m={0}
+									px="md"
+									py="xs"
+									style={{
+										border: "none",
+										borderTop: "1px solid var(--mantine-color-dark-2)",
+										borderRadius: 0,
+									}}
+								>
+									{staticMenuList.map(([name, href]) => (
+										<MenuItem
+											className={classes.menuItem}
+											component="li"
+											key={name}
+											px={0}
+											py="xs"
+											style={{ listStyle: "none" }}
 										>
-											{name}
-										</Anchor>
-									</MenuItem>
-								))}
-								<Suspense>
-									<UserMenuMobile />
-								</Suspense>
-							</MenuDropdown>
-						</Menu>
+											<Anchor
+												className={classes.headerAnchor}
+												component={Link}
+												display="block"
+												href={href}
+												size="sm"
+												underline="never"
+											>
+												{name}
+											</Anchor>
+										</MenuItem>
+									))}
+									<Suspense>
+										<UserMenuMobile />
+									</Suspense>
+								</MenuDropdown>
+							</Menu>
+						</Box>
 						<Group component="ul" gap="xl" m={0} p={0} visibleFrom="sm">
 							{staticMenuList.map(([name, href]) => (
 								<Box component="li" key={name} style={{ listStyle: "none" }}>
@@ -91,7 +94,7 @@ export const Header = () => {
 							</Suspense>
 						</Group>
 					</Box>
-				</Flex>
+				</Group>
 			</Container>
 		</AppShellHeader>
 	);
